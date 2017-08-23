9cc579c50f28e469d19af7045391d7fcd786ab91
Alexander J. Lallier <alexanderlallier@gmail.com>
2017-08-23 14:33:27 -0400

    Added `ul`s to wrap `li` elements so the tags are semantic.

--- publish.js
+++ publish.js
@@ -416,7 +416,7 @@ function buildMemberNav(items, itemHeading, itemsSeen) {
 
             directory = directory.charAt(0).toUpperCase() + directory.slice(1) // Uppercase first letter of directory name
 
-            nav += '<h3>' + directory + '</h3>';
+            nav += '</ul><h3>' + directory + '</h3><ul>';
           }
 
           itemNameSpliced = item.name.substring(item.name.split('/')[0].length).slice(1) + '.js';
@@ -427,7 +427,7 @@ function buildMemberNav(items, itemHeading, itemsSeen) {
           if (lastGroup !== 'root') {
             lastGroup = 'root';
 
-            nav += '<h3>Root</h3>';
+            nav += '</ul><h3>Root</h3><ul>';
           }
           nav += '<li>' + item.name + '.js' + '</li>';
         }
